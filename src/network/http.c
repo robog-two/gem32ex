b1bdae41dec5e28e5e6a3a64e04fc1d8af005fed
Samuel Knight <robogamingbysam@icloud.com>
2025-12-30 20:52:32 -0600

    More logging for https

--- src/network/http.c
+++ src/network/http.c
@@ -15,57 +15,91 @@ void network_response_free(network_response_t *res) {
     }
 }
 
+static void log_last_error(const char *context) {
+    DWORD err = GetLastError();
+    char buffer[1024];
+    DWORD len = FormatMessageA(
+        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_FROM_HMODULE,
+        GetModuleHandleA("wininet.dll"),
+        err,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        buffer,
+        sizeof(buffer),
+        NULL
+    );
+    if (len > 0) {
+        // Remove trailing newlines
+        while (len > 0 && (buffer[len-1] == '\r' || buffer[len-1] == '\n')) {
+            buffer[--len] = '\0';
+        }
+        LOG_ERROR("%s failed: %s (%lu)", context, buffer, err);
+    } else {
+        LOG_ERROR("%s failed: Unknown error %lu", context, err);
+    }
+}
+
 static network_response_t* perform_http_request(const char *url, const char *method, const char *body, const char *content_type) {
-    HINTERNET hInternet = InternetOpen("Gem32Browser/1.0", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
-    if (!hInternet) return NULL;
+    LOG_INFO("HTTP %s %s", method, url);
+
+    HINTERNET hInternet = InternetOpen("Gem32Browser/1.0", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+    if (!hInternet) {
+        log_last_error("InternetOpen");
+        return NULL;
+    }
 
     URL_COMPONENTS urlComp = {0};
     urlComp.dwStructSize = sizeof(urlComp);
     
-    // Allocate buffers for cracking URL
     char host[256] = {0};
     char path[2048] = {0};
+    char extra[1024] = {0};
     
     urlComp.lpszHostName = host;
     urlComp.dwHostNameLength = sizeof(host);
     urlComp.lpszUrlPath = path;
     urlComp.dwUrlPathLength = sizeof(path);
-    urlComp.dwSchemeLength = 1; // Set to non-zero to crack scheme
+    urlComp.lpszExtraInfo = extra;
+    urlComp.dwExtraInfoLength = sizeof(extra);
+    urlComp.dwSchemeLength = 1;
 
     if (!InternetCrackUrl(url, 0, 0, &urlComp)) {
-        LOG_ERROR("InternetCrackUrl failed for %s", url);
+        log_last_error("InternetCrackUrl");
         InternetCloseHandle(hInternet);
         return NULL;
     }
 
-    // Default path if empty
-    if (strlen(path) == 0) strcpy(path, "/");
+    // Combine path and extra info (query params)
+    char full_path[3072];
+    strncpy(full_path, path, sizeof(full_path)-1);
+    if (strlen(extra) > 0) {
+        strncat(full_path, extra, sizeof(full_path) - strlen(full_path) - 1);
+    }
+    if (strlen(full_path) == 0) strcpy(full_path, "/");
 
-    LOG_DEBUG("Cracked URL: Host=%s, Port=%d, Path=%s, Scheme=%d", host, urlComp.nPort, path, urlComp.nScheme);
+    LOG_DEBUG("Connecting to %s:%d (Scheme: %d)", host, urlComp.nPort, urlComp.nScheme);
 
     HINTERNET hConnect = InternetConnect(hInternet, host, urlComp.nPort, NULL, NULL, INTERNET_SERVICE_HTTP, 0, 0);
     if (!hConnect) {
-        LOG_ERROR("InternetConnect failed with error %lu", GetLastError());
+        log_last_error("InternetConnect");
         InternetCloseHandle(hInternet);
         return NULL;
     }
 
-    DWORD flags = INTERNET_FLAG_RELOAD | INTERNET_FLAG_NO_CACHE_WRITE;
+    DWORD flags = INTERNET_FLAG_RELOAD | INTERNET_FLAG_NO_CACHE_WRITE | INTERNET_FLAG_NO_UI | INTERNET_FLAG_NO_COOKIES;
     if (urlComp.nScheme == INTERNET_SCHEME_HTTPS) {
         flags |= INTERNET_FLAG_SECURE;
         flags |= INTERNET_FLAG_IGNORE_CERT_CN_INVALID;
         flags |= INTERNET_FLAG_IGNORE_CERT_DATE_INVALID;
     }
 
-    HINTERNET hRequest = HttpOpenRequest(hConnect, method, path, NULL, NULL, NULL, flags, 0);
+    HINTERNET hRequest = HttpOpenRequest(hConnect, method, full_path, NULL, NULL, NULL, flags, 0);
     if (!hRequest) {
-        LOG_ERROR("HttpOpenRequest failed with error %lu", GetLastError());
+        log_last_error("HttpOpenRequest");
         InternetCloseHandle(hConnect);
         InternetCloseHandle(hInternet);
         return NULL;
     }
 
-    // Add headers if POST
     const char *headers = NULL;
     DWORD headersLen = 0;
     char headerBuf[256];
@@ -73,16 +107,13 @@ static network_response_t* perform_http_request(const char *url, const char *met
     if (body && content_type) {
         snprintf(headerBuf, sizeof(headerBuf), "Content-Type: %s\r\n", content_type);
         headers = headerBuf;
-        headersLen = strlen(headers);
+        headersLen = (DWORD)strlen(headers);
     }
 
-    BOOL sent = HttpSendRequest(hRequest, headers, headersLen, (LPVOID)body, body ? (DWORD)strlen(body) : 0);
+    if (body) LOG_DEBUG("HTTP Body: %s", body);
 
-    LOG_INFO("HTTP %s %s", method, url);
-    if (body) LOG_DEBUG("HTTP BODY: %s", body);
-
-    if (!sent) {
-        LOG_ERROR("HttpSendRequest failed with error %lu", GetLastError());
+    if (!HttpSendRequest(hRequest, headers, headersLen, (LPVOID)body, body ? (DWORD)strlen(body) : 0)) {
+        log_last_error("HttpSendRequest");
         InternetCloseHandle(hRequest);
         InternetCloseHandle(hConnect);
         InternetCloseHandle(hInternet);
